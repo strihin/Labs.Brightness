77f2759bc8fee206af894735f1f72d5ce2f51b96
Aleksandr Strihin <aistrigin92@gmail.com>
2017-01-24 07:54:54 +0200

    first sprint

---
+++ bright/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace bright
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(428, 12);
+            this.trackBar1.Maximum = 127;
+            this.trackBar1.Minimum = -127;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(300, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(428, 63);
+            this.trackBar2.Maximum = 127;
+            this.trackBar2.Minimum = -127;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(300, 45);
+            this.trackBar2.TabIndex = 2;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(428, 114);
+            this.trackBar3.Maximum = 127;
+            this.trackBar3.Minimum = -127;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(300, 45);
+            this.trackBar3.TabIndex = 3;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::bright.Properties.Resources.exam;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(401, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(439, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Серый1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(581, 166);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Серый2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 329);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ bright/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+
+namespace bright
+{
+    public partial class Form1 : Form
+    {
+        Bitmap originBmp;
+        public Form1()
+        {
+            InitializeComponent();
+            originBmp = new Bitmap(pictureBox1.Image);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            double value = trackBar1.Value;
+            if (value != 0)
+            {
+                Bitmap bmp = new Bitmap(originBmp);
+                Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+                BitmapData bmpData = bmp.LockBits(rect, ImageLockMode.ReadWrite, bmp.PixelFormat);
+                IntPtr ptr = bmpData.Scan0;
+                byte[] rgbValues = new byte[bmpData.Stride * bmp.Height];
+                Marshal.Copy(ptr, rgbValues, 0, rgbValues.Length);
+                for (int i = 0; i < rgbValues.Length; i += 4)
+                {
+                    double b = rgbValues[i];
+                    b += value;
+                    if (b > 255)
+                        b = 255;
+                    else if (b < 0)
+                        b = 0;
+                    rgbValues[i] = Convert.ToByte(b);
+
+                }
+                Marshal.Copy(rgbValues, 0, ptr, rgbValues.Length);
+                bmp.UnlockBits(bmpData);
+                pictureBox1.Image = bmp;
+            }
+            else
+            {
+                pictureBox1.Image = originBmp;
+            }
+            
+        
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            double value = trackBar2.Value;
+            if (value != 0)
+            {
+                Bitmap bmp = new Bitmap(originBmp);
+                Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+                BitmapData bmpData = bmp.LockBits(rect, ImageLockMode.ReadWrite, bmp.PixelFormat);
+                IntPtr ptr = bmpData.Scan0;
+                byte[] rgbValues = new byte[bmpData.Stride * bmp.Height];
+                Marshal.Copy(ptr, rgbValues, 0, rgbValues.Length);
+                for (int i = 0; i < rgbValues.Length; i += 4)
+                {
+                    double g = rgbValues[i+1];
+                    g += value;
+                    if (g > 255)
+                        g = 255;
+                    else if (g < 0)
+                        g = 0;
+                    rgbValues[i+1] = Convert.ToByte(g);
+                }
+                Marshal.Copy(rgbValues, 0, ptr, rgbValues.Length);
+                bmp.UnlockBits(bmpData);
+                pictureBox1.Image = bmp;
+            }
+            else
+            {
+                pictureBox1.Image = originBmp;
+            }
+
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            double value = trackBar3.Value;
+            if (value != 0)
+            {
+                Bitmap bmp = new Bitmap(originBmp);
+                Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+                BitmapData bmpData = bmp.LockBits(rect, ImageLockMode.ReadWrite, bmp.PixelFormat);
+                IntPtr ptr = bmpData.Scan0;
+                byte[] rgbValues = new byte[bmpData.Stride * bmp.Height];
+                Marshal.Copy(ptr, rgbValues, 0, rgbValues.Length);
+                for (int i = 0; i < rgbValues.Length; i += 4)
+                {
+                    double r = rgbValues[i+2];
+                    r += value;
+                    if (r > 255)
+                        r = 255;
+                    else if (r < 0)
+                        r = 0;
+                    rgbValues[i+2] = Convert.ToByte(r);
+                }
+                Marshal.Copy(rgbValues, 0, ptr, rgbValues.Length);
+                bmp.UnlockBits(bmpData);
+                pictureBox1.Image = bmp;
+            }
+            else
+            {
+                pictureBox1.Image = originBmp;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(originBmp);
+            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            BitmapData bmpData = bmp.LockBits(rect, ImageLockMode.ReadWrite, bmp.PixelFormat);
+            IntPtr ptr = bmpData.Scan0;
+            byte[] rgbValues = new byte[bmpData.Stride * bmp.Height];
+            Marshal.Copy(ptr, rgbValues, 0, rgbValues.Length);
+            double I;
+            for (int i = 0; i < rgbValues.Length; i += 4)
+            {
+                I = 0.299 * rgbValues[i + 2] + 0.587 * rgbValues[i + 1] + 0.114 * rgbValues[i];
+                if (I > 255)
+                    I = 255;
+                rgbValues[i] = Convert.ToByte(I);
+                rgbValues[i+1] = Convert.ToByte(I);
+                rgbValues[i+2] = Convert.ToByte(I);
+            }
+            Marshal.Copy(rgbValues, 0, ptr, rgbValues.Length);
+            bmp.UnlockBits(bmpData);
+            pictureBox1.Image = bmp;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(originBmp);
+            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            BitmapData bmpData = bmp.LockBits(rect, ImageLockMode.ReadWrite, bmp.PixelFormat);
+            IntPtr ptr = bmpData.Scan0;
+            byte[] rgbValues = new byte[bmpData.Stride * bmp.Height];
+            Marshal.Copy(ptr, rgbValues, 0, rgbValues.Length);
+            double I;
+            for (int i = 0; i < rgbValues.Length; i += 4)
+            {
+                I = 0.22 * rgbValues[i + 2] + 0.76 * rgbValues[i + 1] + 0.02 * rgbValues[i];
+                if (I > 255)
+                    I = 255;
+                rgbValues[i] = Convert.ToByte(I);
+                rgbValues[i + 1] = Convert.ToByte(I);
+                rgbValues[i + 2] = Convert.ToByte(I);
+            }
+            Marshal.Copy(rgbValues, 0, ptr, rgbValues.Length);
+            bmp.UnlockBits(bmpData);
+            pictureBox1.Image = bmp;
+        }
+    }
+}
